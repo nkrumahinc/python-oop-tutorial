5e64518c1ac8b191711672984cc6d20f1b588de5
nkrumahthis <nkrumahthis@gmail.com>
2020-12-13 00:36:19 +0000

    recognize __main__

--- file_two.py
+++ file_two.py
@@ -1,3 +1,8 @@
 # python module to import
 
 print("file two __name__ is set to: {}".format(__name__))
+
+if(__name__ == "__main__"):
+    print("file two executed when ran directly")
+else:
+    print("file two executed when imported")
