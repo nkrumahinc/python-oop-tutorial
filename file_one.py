033779c5b357d16b53f7813113bd56dd598a4f19
nkrumahthis <nkrumahthis@gmail.com>
2020-12-13 00:38:16 +0000

    functions added but does not run

--- file_one.py
+++ file_one.py
@@ -3,6 +3,15 @@ import file_two
 
 print("File one __name__ is set to: {}".format(__name__))
 
+
+def function_one():
+    print("Function one is executed")
+
+
+def function_two():
+    print("Function two is executed")
+
+
 if __name__ == "__main__":
     print("File one executed when ran directly")
 else:
--- file_two.py
+++ file_two.py
@@ -2,6 +2,11 @@
 
 print("file two __name__ is set to: {}".format(__name__))
 
+
+def function_three():
+    print("Function three is executed")
+
+
 if(__name__ == "__main__"):
     print("file two executed when ran directly")
 else:
