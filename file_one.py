a3c4c4a20c00abbf742b378bd3faaf60bd2af456
nkrumahthis <nkrumahthis@gmail.com>
2020-12-13 00:32:12 +0000

    the __name__ variable in file_two is file_two

--- file_one.py
+++ file_one.py
@@ -1,3 +1,4 @@
-# Python file one module
+# Python module to execute
+import file_two
 
 print("File one __name__ is set to: {}".format(__name__))
---
+++ file_two.py
@@ -0,0 +1,3 @@
+# python module to import
+
+print("file two __name__ is set to: {}".format(__name__))
